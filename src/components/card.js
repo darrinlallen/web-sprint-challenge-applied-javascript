8d7f6aaeead56099197539162b2d9d30ee49cd4d
darrin allen <darrin.lovard.allen@gmail.com>
2022-12-14 21:56:04 -0800

    finished 5

--- src/components/card.js
+++ src/components/card.js
@@ -18,48 +18,48 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-  const cardWrapper = document.createElement('div');
-        cardWrapper.classList.add('card')
-  
+
   
 let arr =[];
 for (var key in article){
   arr.push(article[key])
 }
+let arr2 =[]
+arr.forEach(mobile => {
+  for (let key in mobile) {
+    arr2.push(mobile[key])
+  }
+})
 
-       
-      
-          
 
-  
+
+        const cardWrapper = document.createElement('div');
+
         const headline = document.createElement('div')
         const author = document.createElement('div')
         const imgContainer = document.createElement('div')
         const authorPhoto = document.createElement('img')
         const authorName = document.createElement('span')
-
         headline.classList.add('headline');
          author.classList.add('author')
        imgContainer.classList.add('img-container')
-
-       headline.textContent = article.headline;
+          cardWrapper.classList.add('card')
+       headline.textContent = article.headline
        authorPhoto.src = article.authorPhoto;
        authorName.textContent = `By ${article.authorName}`
        
        cardWrapper.appendChild(headline);
-       author.appendChild(imgContainer)
-       imgContainer.appendChild(authorPhoto)
-       author.appendChild(authorName)  
        cardWrapper.appendChild(author)
-        
-
-          
+       imgContainer.appendChild(authorPhoto)
+       author.appendChild(imgContainer)
+       author.appendChild(authorName)  
+       
+      
+      
   
-  
-
-      console.log(arr)
-    console.log(cardWrapper);
+          console.log()
     return cardWrapper;
+   
     }
     
 const cardAppender = (selector) => {
@@ -71,9 +71,27 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  const end =  document.querySelector(selector)
+
   axios.get('http://localhost:5001/api/articles')
-  .then( resp => { 
-    document.querySelector(selector).appendChild(Card(resp.data));
+  .then( resp => {
+    let arr =[];
+
+    for (var key in resp.data){
+      arr.push(resp.data[key])
+    }
+    let arr2 =[]
+    arr.forEach(mobile => {
+      for (let key in mobile) {
+        arr2.push(mobile[key])
+      }
+    })
+  for (let i =0 ; i < arr2.length; i++){
+    for (let j=0; j < arr2[i].length; j++){
+      var kid = Card(arr2[i][j])
+      end.appendChild(kid)
+    }
+  }
   })
   .catch(err => console.error(err))
   }
